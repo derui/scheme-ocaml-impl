60e45aff0cb68f96db510270814d8431a87c578e
derui <derutakayu@gmail.com>
2021-02-02 21:41:21 +0900

    feat: implement some of many combinators

--- src/syntax_rule_lib.ml
+++ src/syntax_rule_lib.ml
@@ -4,7 +4,7 @@ module Pr = Printer
 type syntax_rule = { symbol_table : (string, string) Hashtbl.t }
 
 type pattern =
-  [ `Symbol
+  [ `Symbol     of string
   | `Nested     of pattern
   | `Literal    of T.data
   | `Underscore
@@ -15,8 +15,9 @@ module Pattern_matcher = struct
   type t = { syntax_rule : syntax_rule }
 end
 
-(* module to parse syntax-rules's pattern. This module define some parser combinator to be used in parsing syntax-rule. *)
-module Rule_parser = struct
+(* module to parse scheme's list as data type defined in OCaml. This module define some parser combinator to be used in
+   parsing syntax-rule. *)
+module List_parser = struct
   (* The type of parser *)
   type 'a t = T.data -> ('a * T.data, string) result
 
@@ -44,29 +45,67 @@ module Rule_parser = struct
     let ( let+ ) = apply
   end
 
-  let get_exp = function
+  open Let_syntax
+  open Infix
+
+  (* Apply [p1] and [p2] sequentially and use right result *)
+  let ( *> ) p p2 = Infix.((fun _ y -> y) <$> p <*> p2)
+
+  (* Apply [p1] and [p2] sequentially and use left result *)
+  let ( *< ) p p2 = Infix.((fun x _ -> x) <$> p <*> p2)
+
+  let expression = function
     | T.Empty_list         -> Error "empty list"
     | Cons (v, Empty_list) -> Ok (v, T.Empty_list)
     | Cons (v, rest)       -> Ok (v, rest)
     | _ as v               -> Ok (v, T.Empty_list)
 
-  let empty _ = Error "empty"
+  let zero _ = Error "empty"
 
-  let satisfy p data = match p data with Error _ -> Ok (None, data) | Ok (v, _) -> Ok (Some v, data)
+  let choice p q data =
+    let p = p data in
+    let q = q data in
+    match (p, q) with
+    | Error _, Error _              -> Error "can not choice"
+    | Error _, Ok v | Ok v, Error _ -> Ok v
+    | Ok p, Ok _                    -> Ok p
+
+  (* combinator to choice *)
+  let ( <|> ) = choice
+
+  let satisfy p =
+    let* v = expression in
+    if p v then pure v else zero
 
   let many : 'a t -> 'a list t =
    fun p ->
-    let open Let_syntax in
+    let p = (fun v -> [ v ]) <$> p in
     let rec many' accum =
-      let* v = satisfy p in
-      match v with None -> pure (List.rev accum) | Some v -> many' (v :: accum)
+      let* v = p <|> pure [] in
+      match v with [] -> List.rev accum |> pure | v :: _ -> many' (v :: accum)
     in
-    let* ps = many' [] in
-    match ps with [] -> pure [] | _ -> pure ps
+    let* ps = many' [] <|> pure [] in
+    pure ps
 
   let many1 p =
-    let open Let_syntax in
     let* p' = p in
     let* ps = many p in
     fun data_list -> Ok (p' :: ps, data_list)
+
+  (* chain one or more repeated operator to result of parser. *)
+  let chainl1 : 'a t -> ('a -> 'a -> 'a) t -> 'a t =
+   fun p op ->
+    let rec chain_rest a =
+      let result_of_cycle =
+        let* f = op in
+        let* v' = p in
+        chain_rest (f a v')
+      in
+      result_of_cycle <|> pure a
+    in
+    let* a = p in
+    chain_rest a
+
+  (* chain zero or more repeated operator to result of parser. *)
+  let chainl p op a = chainl1 p op <|> pure a
 end
--- test/syntax_rule_lib_test.ml
+++ test/syntax_rule_lib_test.ml
@@ -4,17 +4,45 @@ module Pr = Ocaml_scheme.Printer
 
 let exp_pp = Alcotest.testable Pr.pp ( = )
 
-let parser_pp = Alcotest.(result (pair exp_pp @@ exp_pp) string)
+let parser_pp p = Alcotest.(result (pair p @@ exp_pp) string)
 
 let list_to_scheme_list v = List.rev v |> List.fold_left (fun accum v -> T.Cons (v, accum)) T.Empty_list
 
-let rule_parser_tests =
+let list_parser_tests =
   [
-    Alcotest.test_case "Rule Parser: parser a expression from list" `Quick (fun () ->
+    Alcotest.test_case "List Parser: parser a expression from list" `Quick (fun () ->
         let list = [ T.Symbol "foo" ] |> list_to_scheme_list in
-        let actual = S.Rule_parser.get_exp list in
+        let actual = S.List_parser.expression list in
         let expected = Ok (T.Symbol "foo", T.Empty_list) in
-        Alcotest.(check parser_pp) "simple" actual expected);
+        Alcotest.(check @@ parser_pp exp_pp) "simple" actual expected);
+    Alcotest.test_case "List Parser: many combinator get zero or more repeated list" `Quick (fun () ->
+        let p =
+          S.List_parser.Infix.(
+            (function T.Symbol s -> String.uppercase_ascii s | _ -> "other") <$> S.List_parser.expression)
+        in
+        let list = [ T.Symbol "foo"; T.Symbol "bar" ] |> list_to_scheme_list in
+        let actual = S.List_parser.many p list in
+        let expected = Ok ([ "FOO"; "BAR" ], T.Empty_list) in
+        Alcotest.(check @@ parser_pp @@ list string) "simple" actual expected);
+    Alcotest.test_case "List Parser: many1 combinator get  one or more repeated list" `Quick (fun () ->
+        let p =
+          S.List_parser.Infix.(
+            (function T.Symbol s -> String.uppercase_ascii s | _ -> "other") <$> S.List_parser.expression)
+        in
+        let list = [ T.Symbol "foo"; T.Symbol "bar" ] |> list_to_scheme_list in
+        let actual = S.List_parser.many p list in
+        let expected = Ok ([ "FOO"; "BAR" ], T.Empty_list) in
+        Alcotest.(check @@ parser_pp @@ list string) "simple" actual expected);
+    Alcotest.test_case "List Parser: chainl1 chains operator between parsers" `Quick (fun () ->
+        let p =
+          S.List_parser.Infix.(
+            (function T.Symbol s -> String.uppercase_ascii s | _ -> "other") <$> S.List_parser.expression)
+        in
+        let op = S.List_parser.pure @@ fun a b -> a ^ "___" ^ b in
+        let list = [ T.Symbol "foo"; T.Symbol "bar" ] |> list_to_scheme_list in
+        let actual = S.List_parser.(chainl1 p op) list in
+        let expected = Ok ("FOO___BAR", T.Empty_list) in
+        Alcotest.(check @@ parser_pp string) "simple" actual expected);
   ]
 
-let tests = rule_parser_tests
+let tests = list_parser_tests
